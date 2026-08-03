7b30db7c44824f27a2d0dd8d8a633ba4315b1e88
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2026-08-03 12:01:45 -0300

    update produtos api

--- frontend/loja/pages/Produtos.jsx
+++ frontend/loja/pages/Produtos.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Link } from 'react-router'
 
 export const Produtos = ({dados,setDados}) => {
+    
+   if (!dados) {
+    return <h1>Carregando...</h1>;
+  }
   return (
-    <div className='container grid grid-cols-3 gap-10 mt-30'>
+    <div className='container grid grid-cols-2 gap-10 mt-30'>
         {dados.map((dado)=>(
             <div key={dado.id}>
                 <Link to={`/produto/${dado._id}`}>
-                    <img src={dado.image} alt={dado.nome} className='w-100 h-100' />
+                    <img src={dado.image} alt={dado.nome} className='w-150 h-100 object-contain' />
                     <div>
                         <h3>{dado.nome}</h3>
                         <span>R$ {dado.preco}</span>
